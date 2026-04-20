439e1b9c3064fab01d2afd835b9987a56acb467f
d1000m3000-sys <d1000m3000@gmail.com>
2026-04-20 14:43:13 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,22 @@
-const CACHE_NAME = "xnots-v1";
+const CACHE = "xnots-pro";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll([
+"./",
+"./index.html",
+"./manifest.json",
+"./icon.png"
+]);
+})
+);
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res || fetch(e.request);
+})
+);
 });
